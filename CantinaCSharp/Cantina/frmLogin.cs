bc664759b760a1f1e8c67860607b926a64b9a6a0
JosueGraao <145507671+JosueGraao@users.noreply.github.com>
2024-08-23 22:10:23 -0300

    Tá tudo errado

---
+++ CantinaCSharp/Cantina/frmLogin.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Cantina
+{
+    public partial class frmLogin : Form
+    {
+        public frmLogin()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSair_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        public void limparCampos() 
+        {
+            txtUsuario.Clear();
+            txtSenha.Text = "";
+            txtUsuario.Focus();
+        }
+        
+        private void btnEntrar_Click(object sender, EventArgs e)
+        {
+            string usuario, senha;
+
+            usuario = txtUsuario.Text;
+            senha = txtSenha.Text;
+
+            if (usuario.Equals("etecia") && senha.Equals("etecia"))
+            {
+                frmMenuPrincipal abrir = new frmMenuPrincipal();
+                abrir.Show();
+                this.Hide();
+            }
+            else
+            {
+                MessageBox.Show("Usuário ou senha incorretos.", 
+                    "Mensagem do sistema", 
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Error, 
+                    MessageBoxDefaultButton.Button1);
+                limparCampos();
+            }
+        }
+
+        private void txtUsuario_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.Enter) 
+            {
+                txtSenha.Focus();
+            }
+        }
+
+        private void txtSenha_TextChanged(object sender, EventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                txtSenha.Focus();
+            }
+        }
+    }
+}
